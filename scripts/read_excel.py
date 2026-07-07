6b8e8f209bd455cbb05ff8a1e12ed705a9aced5c
nam-chu <namhoangchu@gmail.com>
2026-07-07 17:42:00 -0400

    cleaning data

--- scripts/read_excel.py
+++ scripts/read_excel.py
@@ -1,16 +1,13 @@
 import pandas as pd
-import numpy as np
-import os
-
 
 path =  "canada_fsa_climate_footprint.csv"
 
-def replace_empty(file):
-    df = pd.read_csv(file)
-    total_missing = df.isna().sum().sum()
-    missing_columns = df.isna().sum()
-    if total_
-    print(total_missing)
-    print(missing_columns)
-replace_empty(path)
+def clean_data(path):
+    '''
+    Reads in a file and cleans it 
+    '''
+    df = read.csv(path, header=3, thousands=",")
+
+
+clean_data(path)
 
